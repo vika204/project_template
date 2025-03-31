7c75eb2c0a2625ff60462e4de2e4e0f524ac6a45
Vika Romaniuk <vikaromaniuk345@gmail.com>
2025-03-31 23:30:26 +0300

    Created test_input.py with basic unittest structure

---
+++ tests/test_input.py
@@ -0,0 +1,8 @@
+import unittest
+from app.io.input import read_from_file_builtin, read_from_file_pandas
+
+class TestInputFunctions(unittest.TestCase):
+    pass
+
+if __name__ == "__main__":
+    unittest.main()
