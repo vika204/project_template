6ddfe0cb291713c08ae6f6aa2c48a987a1bd442c
Vika Romaniuk <vikaromaniuk345@gmail.com>
2025-03-31 23:19:59 +0300

    Added empty functions with docstrings in input.py and output.py

---
+++ app/io/input.py
@@ -0,0 +1,11 @@
+def read_from_console():
+    """Reads text input from the console."""
+    pass
+
+def read_from_file_builtin():
+    """Reads text from a file using Python's built-in capabilities."""
+    pass
+
+def read_from_file_pandas():
+    """Reads data from a file using pandas library."""
+    pass
---
+++ app/io/output.py
@@ -0,0 +1,7 @@
+def print_to_console():
+    """Prints text to the console."""
+    pass
+
+def write_to_file_builtin():
+    """Writes text to a file using Python's built-in capabilities."""
+    pass
